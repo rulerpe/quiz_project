f0bb27a16ed85b4178fb16f40f3f0bc576d10efb
Peter <neetisthebest@gmail.com>
2015-03-03 23:27:56 -0800

    fix loop

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 (function(){
 	
-	var data =  [{ 	question: "When is new year",
+	var data =  [{ 	
+					question: "When is new year",
 					choices: ["March 1st","January 1st","Octorber 5th","June 30th"],
 					answer: 1 },
 					{ question: "Where is China",
@@ -11,16 +12,13 @@
 					answer: 3 }];
 	var qustionLength = data.length;
 	var counter = 0;
-	var playerAnswer ;
+	var playerAnswer = [];
 	var score = 0;
 
 	var form = document.getElementById("content");
 	var q = document.getElementById("qustion");
 	var allChoice = form.getElementsByTagName("label");
-	q.textContent  = data[counter].question;
-	for(var i=0; i<4; i++){
-		allChoice[i].textContent = data[counter].choices[i];
-	}
+	q.textContent  = "Welcome click next to start";
 	form.addEventListener("submit", change,false);
 
 
@@ -28,25 +26,36 @@
 	function change(e){
 		e.preventDefault();
 		if(counter < data.length){
-			q.textContent  = data[counter+1].question;
+			q.textContent  = data[counter].question;
 			for(var i=0; i<4; i++){
-				allChoice[i].textContent = data[counter+1].choices[i];
+				allChoice[i].textContent = data[counter].choices[i];
 			}
-			for(var i=0; i<4; i++){
-				if( form.elements[i].checked){
-					playerAnswer = i;
-					break;
+			if (counter!=0 ){
+				for(var i=0; i<4; i++){
+					if( form.elements[i].checked){
+						playerAnswer.push(i);
+						break;
+					}
 				}
-			}
-			
-			
-			if (playerAnswer == data[counter].answer){
-				
-				score += 1;
+				if (playerAnswer[counter-1] == data[counter-1].answer){	
+					score += 1;
+				}
 			}	
 			counter++;	
 		}else {
+			for(var i=0; i<4; i++){
+					if( form.elements[i].checked){
+						playerAnswer.push(i);
+						break;
+					}
+				}
+				if (playerAnswer[counter-1] == data[counter-1].answer){	
+					score += 1;
+				}
 			q.textContent  = "Your total score is "+score;
+			for(var i=0; i<4; i++){
+				allChoice[i].textContent = "";
+			}
 		}
 	}
 
